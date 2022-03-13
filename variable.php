1271a38ecd36100956586df19e541c909d127340
TrizzQQ <trissna007@gmail.com>
2022-03-13 07:55:14 +0700

    3

---
+++ variable.php
@@ -0,0 +1,18 @@
+<?php
+	$a = 4;
+	$b = 6;
+	$c = 8;
+	echo "$a, $b, $c";
+	echo '<br/>';
+	echo 'Penjumlahan = ';
+	$plus = $a + $b + $c;
+	echo "$plus";
+	echo '<br/>';
+	echo 'Pengurangan = ';
+	$minus = $a - $b - $c;
+	echo "$minus";
+	echo '<br/>';
+	echo 'Perkalian = ';
+	$kali = $a * $b * $c;
+	echo "$kali";
+?>
